ee0e3104d262d467d4787fb41ce3e1d2e5def353
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2012-09-22 18:37:26 +0200

    [MrProper] airing Equipement class code

--- src/com/gameplay/items/Equipement.java
+++ src/com/gameplay/items/Equipement.java
@@ -9,17 +9,18 @@ import com.gameplay.Caracteristique;
 public class Equipement extends Item
 {
 	private String type;
+
 	public Equipement(String nom, String type, String description, Image icon, Image desc,
 			HashMap<Caracteristique, Integer> effets, int poids) 
 	{
 		super(nom, description, icon, desc, effets, poids);
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
-	
 }
