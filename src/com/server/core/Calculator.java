4170e14c0e1ffaff7855905a6798285709b78d1f
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2012-09-22 18:52:06 +0200

    [MrProper] Fix indentation in Calculator

--- src/com/server/core/Calculator.java
+++ src/com/server/core/Calculator.java
@@ -2,7 +2,6 @@ package com.server.core;
 
 import java.io.IOException;
 
-
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
@@ -18,105 +17,97 @@ import com.server.core.functions.StateFunction;
 
 public class Calculator implements Runnable
 {
-    private static HashMap <String,Functionable> dictfunctions = new HashMap<String,Functionable>();
-    private List<ClientList> cl;
-    private Thread t;
-    
-   public Calculator(MapManager mapManager, List<ClientList> cl)
-   {
-	   this.cl = cl;
-	   
-       //On ajoute les fonctions
-     /* dictfunctions.put("s",new StateFunction());
+	private static HashMap <String,Functionable> dictfunctions = new HashMap<String,Functionable>();
+	private List<ClientList> cl;
+	private Thread t;
+
+	public Calculator(MapManager mapManager, List<ClientList> cl)
+	{
+		this.cl = cl;
+
+		//On ajoute les fonctions
+		/*
       dictfunctions.put("a",new AttackFunction());
       dictfunctions.put("l",new LootFunction());
       dictfunctions.put("t",new TakeFunction());
       dictfunctions.put("m",new MoveFunction());
-      dictfunctions.put("i",new InfoFunction());
       dictfunctions.put("o",new InvitFunction());
       dictfunctions.put("r",new ResultInvitFunction());
-      dictfunctions.put("c",new ConnectFunction());
-      dictfunctions.put("gl",new ListInvitFunction());
-      dictfunctions.put("sa",new SayFunction());*/
-	   
-	   dictfunctions.put("s",new StateFunction());
-	   dictfunctions.put("i",new InfoFunction());
-	   dictfunctions.put("c",new ConnectFunction());
-	   dictfunctions.put("sa",new SayFunction());
-	   dictfunctions.put("lo",new LoadFunction());
-	   
-	   this.t = new Thread(this);
-	   t.start();
-   }
-   
-   public void submit(Client source, String mess)
-   {
-       
-       if(mess!=null && !mess.equals("none") && !mess.isEmpty())
-       {
-           System.out.println("Reception du message :" + mess);
-           String[] temp = mess.split(";");
-           Functionable f = dictfunctions.get(temp[0]);
-           ArrayList<String> tmp = f.doSomething(temp,source.getCompte().getClient_id());
-           for(int i=0;i<tmp.size();i++)
-           {
-           		String s = tmp.get(i);
-           		if(temp[0].equals("c") && s.matches("^[0-9]+$"))
-           		{
-           			source.getCompte().setClient_id(Integer.parseInt(s));
-           		}
-           		else
-           		{
-           			source.sendToClient(s) ;
-	            	if(mess.equals("CONNECT_FAIL"))
-	            		ServerSingleton.getInstance().deconnexion(source);
-           		}
-           }
-       }
-   }
+      dictfunctions.put("gl",new ListInvitFunction());*/
 
-   @Override
-   public void run() 
-   {
-	  while(true)
-	  {
-		  try
-          {
-              // Pour chaque client connecte
-              for(int i = 0; i < cl.size(); i++)
-              {
-            	  for(int j = 0; j < cl.get(i).getClients().size(); j++)
-                  {
-            		  Client c = cl.get(i).getClients().get(j);
-                      try {
-    					this.
-    					submit(
-    							c,
-    							c.receiveFromClient());
-    				} catch (IOException e) {
-    					e.printStackTrace();
-    				}
-                  }
-                 
-              }
-          }
+		dictfunctions.put("s",new StateFunction());
+		dictfunctions.put("i",new InfoFunction());
+		dictfunctions.put("c",new ConnectFunction());
+		dictfunctions.put("sa",new SayFunction());
+		dictfunctions.put("lo",new LoadFunction());
 
-          catch (ConcurrentModificationException e)
-          {
-              /*On eteint le serveur si on a une erreur de modification concurrente.
-              *Car ca voudra dire que j'ai du boulot
-              */
-              ServerSingleton.getInstance().seeToServer(e.getMessage());
-              //l.fatal(e.getMessage());
-          }
-	  }
-   }
+		this.t = new Thread(this);
+		t.start();
+	}
 
-   public List<ClientList> getCl() {
-	   return cl;
-   }
+	public void submit(Client source, String mess)
+	{
 
-   public void setCl(List<ClientList> cl) {
-	   this.cl = cl;
-   }
+		if(mess!=null && !mess.equals("none") && !mess.isEmpty())
+		{
+			System.out.println("Reception du message :" + mess);
+			String[] temp = mess.split(";");
+			Functionable f = dictfunctions.get(temp[0]);
+			ArrayList<String> tmp = f.doSomething(temp,source.getCompte().getClient_id());
+			for(int i=0;i<tmp.size();i++)
+			{
+				String s = tmp.get(i);
+				if(temp[0].equals("c") && s.matches("^[0-9]+$"))
+				{
+					source.getCompte().setClient_id(Integer.parseInt(s));
+				}
+				else
+				{
+					source.sendToClient(s) ;
+					if(mess.equals("CONNECT_FAIL"))
+						ServerSingleton.getInstance().deconnexion(source);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void run() 
+	{
+		while(true)
+		{
+			try
+			{
+				// Pour chaque client connecte
+				for(int i = 0; i < cl.size(); i++)
+				{
+					for(int j = 0; j < cl.get(i).getClients().size(); j++)
+					{
+						Client c = cl.get(i).getClients().get(j);
+						try {
+							this.submit(c,c.receiveFromClient());
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			catch (ConcurrentModificationException e)
+			{
+				/*On eteint le serveur si on a une erreur de modification concurrente.
+				 *Car ca voudra dire que j'ai du boulot
+				 */
+				ServerSingleton.getInstance().seeToServer(e.getMessage());
+				//l.fatal(e.getMessage());
+			}
+		}
+	}
+
+	public List<ClientList> getCl() {
+		return cl;
+	}
+
+	public void setCl(List<ClientList> cl) {
+		this.cl = cl;
+	}
 }
