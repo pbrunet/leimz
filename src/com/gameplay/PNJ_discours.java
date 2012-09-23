9c2eb5852bfe4082570a2ffe54481de67f0dba3c
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2012-09-23 14:11:03 +0200

    [MrProper] cleaning up extra line in pnj discourts

--- src/com/gameplay/PNJ_discours.java
+++ src/com/gameplay/PNJ_discours.java
@@ -8,46 +8,34 @@ public class PNJ_discours
 	private ArrayList<Integer> tree_reponses;
 	private ArrayList<String> reponses;
 	
-	
 	public PNJ_discours(String discours, ArrayList<Integer> treeReponses, ArrayList<String> reponses) 
 	{
 		this.discours = discours;
-		tree_reponses = treeReponses;
+		this.tree_reponses = treeReponses;
 		this.reponses = reponses;
 	}
-
 
 	public String getDiscours() {
 		return discours;
 	}
 
-
 	public void setDiscours(String discours) {
 		this.discours = discours;
 	}
-
 
 	public ArrayList<Integer> getTree_reponses() {
 		return tree_reponses;
 	}
 
-
 	public void setTree_reponses(ArrayList<Integer> treeReponses) {
 		tree_reponses = treeReponses;
 	}
-
 
 	public ArrayList<String> getReponses() {
 		return reponses;
 	}
 
-
 	public void setReponses(ArrayList<String> reponses) {
 		this.reponses = reponses;
 	}
-	
-	
-	
-	
-	
 }
